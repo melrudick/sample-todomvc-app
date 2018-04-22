70e608ff727e6a2b0709e5a8d6c0d062d4f18f3c
Mel Rudick <melissarudick@MacBook-Air.local>
2018-04-22 07:27:11 -0500

    all working through auth except item update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(:email => params[:email])
-    session[:user_id] = user.id
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'sessions/new'
+    end
     #every request is independent and doesn't remember
     # so how do we log in?
-    redirect_to root_path
   end
 
   def destroy
